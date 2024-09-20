7a0f2fe7252d1883aeae601f49db7ab89ef9aac4
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-20 21:20:22 +0800

    initialize tailwind CSS and tested, working

---
+++ Frontend/src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-6xl text-red-600">
+    <p>
+      Hello World
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/quasar/src/**/*.{js,ts,vue}',
+    './node_modules/quasar/src/**/*.sass',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
