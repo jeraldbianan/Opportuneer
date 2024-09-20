a4aeb8d9310dcf25adb137da00edba95f5f66d62
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-20 16:36:01 +0800

    added CORS Policy

---
+++ Backend/Framework/Middleware/CorsMiddleware.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Framework\Middleware;
+
+class CorsMiddleware {
+  public static function handle() {
+    header("Access-Control-Allow-Origin: http://localhost:3000");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Credentials: true");
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+      http_response_code(200);
+      exit;
+    }
+  }
+}
--- Backend/public/index.php
+++ Backend/public/index.php
@@ -1,3 +1,7 @@
 <?php
 require '../helpers.php';
 require basePath('vendor/autoload.php');
+
+use Framework\Middleware\CorsMiddleware;
+
+CorsMiddleware::handle();
