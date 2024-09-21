780f2a3032218bad5d135328a4df68c0b698350c
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-21 12:50:20 +0800

    change CORS origin so that frontend can fetch data from backend

--- Backend/Framework/Middleware/CorsMiddleware.php
+++ Backend/Framework/Middleware/CorsMiddleware.php
@@ -4,7 +4,7 @@ namespace Framework\Middleware;
 
 class CorsMiddleware {
   public static function handle() {
-    header("Access-Control-Allow-Origin: http://localhost:3000");
+    header("Access-Control-Allow-Origin: http://localhost:9000");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header("Access-Control-Allow-Credentials: true");
