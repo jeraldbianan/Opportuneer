945dc6ecee7c63abc187cb25d81d4059b7b74195
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-24 21:45:52 +0800

    converted JobCard to quasar card

--- Frontend/src/components/UI/Cards/JobCard.vue
+++ Frontend/src/components/UI/Cards/JobCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <div
+  <q-card
     class="w-[367px] rounded-lg bg-light-pink/50 p-[30px] transition-all hover:scale-105"
   >
-    <div class="flex items-center gap-3">
+    <q-card-section class="flex items-center gap-3">
       <q-img
         :src="job.logo_url"
         :alt="job.company"
@@ -16,24 +16,28 @@
         </p>
         <p class="font-open-sans text-xl font-bold">{{ job.company }}</p>
       </div>
-    </div>
+    </q-card-section>
+
     <q-separator class="my-6 h-[1px] w-full text-light-blue" />
-    <p class="font-open-sans text-xl font-semibold">{{ job.title }}</p>
-    <p class="mt-4 line-clamp-4 font-open-sans text-base !leading-[140%]">
-      {{ job.description }}
-    </p>
-    <div class="mt-8 flex items-center justify-between">
-      <div class="flex flex-col gap-2 font-open-sans text-xs">
-        <p>{{ job.type }}</p>
-        <p>{{ `₱${job.salary} - ₱${job.secondary_salary}` }}</p>
+
+    <q-card-section>
+      <p class="font-open-sans text-xl font-semibold">{{ job.title }}</p>
+      <p class="mt-4 line-clamp-4 font-open-sans text-base !leading-[140%]">
+        {{ job.description }}
+      </p>
+      <div class="mt-8 flex items-center justify-between">
+        <div class="flex flex-col gap-2 font-open-sans text-xs">
+          <p>{{ job.type }}</p>
+          <p>{{ `₱${job.salary} - ₱${job.secondary_salary}` }}</p>
+        </div>
+        <q-btn
+          unelevated
+          label="Details"
+          class="h-7 w-[94px] bg-dark-blue/70 font-open-sans text-[10px] font-semibold normal-case text-white"
+        />
       </div>
-      <q-btn
-        unelevated
-        label="Details"
-        class="h-7 w-[94px] bg-dark-blue/70 font-open-sans text-[10px] font-semibold normal-case text-white"
-      />
-    </div>
-  </div>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script setup lang="ts">
