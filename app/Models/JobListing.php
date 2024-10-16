ae4ac965aa66e35f499ee902119a5b2b96ef04b7
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-17 02:18:29 +0800

    added some static arrays in JobListing model

--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -8,6 +8,47 @@ use Illuminate\Database\Eloquent\Model;
 class JobListing extends Model {
     use HasFactory;
 
-    public static array $experience = ['entry', 'intermediate', 'senior'];
+    public static array $experience = ['Entry', 'Intermediate', 'Senior'];
     public static array $category = ['IT', 'Finance', 'Sales', 'Marketing'];
+    public static array $type = ['Part-Time', 'Full-Time'];
+    public static array $itTags = [
+        'Frontend Web Development',
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'Backend Development',
+        'PHP',
+        'Laravel',
+        'Vue.js'
+    ];
+    public static array $financeTags = [
+        'Financial Analyst',
+        'Investment Banking',
+        'Accounting',
+        'Auditing',
+        'Tax Consultant',
+        'Risk Management',
+        'Financial Planning',
+        'Corporate Finance'
+    ];
+    public static array $salesTags = [
+        'Account Executive',
+        'Business Development',
+        'Sales Manager',
+        'Sales Representative',
+        'Sales Coordinator',
+        'Lead Generation',
+        'Sales Strategy',
+        'Sales Operations'
+    ];
+    public static array $marketingTags = [
+        'Digital Marketing',
+        'Content Marketing',
+        'SEO Specialist',
+        'Social Media Manager',
+        'Email Marketing',
+        'Marketing Strategy',
+        'Brand Manager',
+        'Marketing Coordinator'
+    ];
 }
