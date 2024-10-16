8be7f2446002213af7fc213269c338fcaaed894c
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-16 22:22:15 +0800

    added static arrays in JobListing model, configure JoblistingFactory, JoblistingSeeder and DatabaseSeeder

--- database/factories/JobListingFactory.php
+++ database/factories/JobListingFactory.php
@@ -2,22 +2,26 @@
 
 namespace Database\Factories;
 
+use App\Models\JobListing;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\JobListing>
  */
-class JobListingFactory extends Factory
-{
+class JobListingFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
         return [
-            //
+            'title' => fake()->jobTitle(),
+            'description' => fake()->paragraph(3, true),
+            'salary' => fake()->numberBetween(5_000, 150_000),
+            'location' => fake()->city(),
+            'category' => fake()->randomElement(JobListing::$category),
+            'experience' => fake()->randomElement(JobListing::$experience),
         ];
     }
 }
--- database/seeders/JobListingSeeder.php
+++ database/seeders/JobListingSeeder.php
@@ -2,16 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\JobListing;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class JobListingSeeder extends Seeder
-{
+class JobListingSeeder extends Seeder {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        //
+    public function run(): void {
+        JobListing::factory(100)->create();
     }
 }
