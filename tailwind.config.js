3d1378530f331833696df2b8cfaa56feca0626c1
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-17 00:18:33 +0800

    added colors, container and fonts in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+                montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
+                "open-sans": ["Open Sans", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "dark-blue": "#6482AD",
+                "light-blue": "#7FA1C3",
+                "white-coffee": "#E2DAD6",
+                "light-pink": "#F5EDED",
+                danger: "#D15151",
+                success: "#25CC72",
+            },
+            maxWidth: {
+                container: "1160px",
             },
         },
     },
