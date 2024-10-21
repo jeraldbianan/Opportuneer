442b8094c1c6133446b9c92a16f9023e0c7537df
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-21 22:49:35 +0800

    feature added, display all jobs from the same employer in job details/job.show page

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -12,4 +12,26 @@
     <x-card class="p-4">
         <x-job-card :$job />
     </x-card>
+
+    <x-card class="mb-4 mt-10 p-4">
+        <h2 class="mb-4 text-lg font-medium font-montserrat">
+            More Jobs at <span class="font-bold">{{ $job->employer->company_name }}</span>
+        </h2>
+        <div class="text-sm">
+            @foreach ($job->employer->jobListings as $otherJob)
+                <div class="mb-4 flex justify-between">
+                    <div>
+                        <div
+                            class="text-sm font-semibold text-dark-blue border-b border-b-transparent hover:border-b-dark-blue">
+                            <a href="{{ route('job-listings.show', $otherJob) }}">{{ $otherJob->title }}</a>
+                        </div>
+                        <div class="text-sm">
+                            {{ $otherJob->created_at->diffForHumans() }}
+                        </div>
+                    </div>
+                    <div class="text-sm">₱{{ number_format($otherJob->salary) }}</div>
+                </div>
+            @endforeach
+        </div>
+    </x-card>
 </x-layout>
