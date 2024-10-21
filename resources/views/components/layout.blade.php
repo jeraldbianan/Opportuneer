e23aec2f82f1b35df21f21665976b4346daf70c3
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-21 16:22:31 +0800

    install alpine.js into the project

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Opportuneer</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
