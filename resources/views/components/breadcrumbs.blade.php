3ced512b6ebc801d5219956c89e9ec5a870af466
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-21 12:45:30 +0800

    added hover effects in the breadcrumbs

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,12 +1,12 @@
 <nav {{ $attributes }}>
     <ul class="flex space-x-2 text-dark-blue items-center text-xs">
-        <li>
+        <li class="border-b border-b-transparent hover:border-b-dark-blue">
             <a href="/">Home</a>
         </li>
 
         @foreach ($links as $label => $link)
             <li>→</li>
-            <li>
+            <li class="border-b border-b-transparent hover:border-b-dark-blue">
                 <a href="{{ $link }}">{{ $label }}</a>
             </li>
         @endforeach
