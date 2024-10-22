b592eb8bd2cc21f2930f7a6724d902449c259aec
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-22 15:55:31 +0800

    develop the navigation bar

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,3 +1,27 @@
-<nav>
-    NAVBAR
-</nav>
+<div class="container p-3">
+    <div class="flex justify-between items-center">
+        <a href="{{ route('home') }}">
+            <x-icons.logo></x-icons.logo>
+        </a>
+        <div class="flex gap-10 items-center">
+            <ul class="flex gap-10 font-semibold text-sm">
+                <li class="hover:text-dark-blue
+                @if (Route::currentRouteName() === 'home') text-dark-blue @endif">
+                    <a href="{{ route('home') }}">Home</a>
+                </li>
+
+                <li class="hover:text-dark-blue
+                @if (Route::currentRouteName() === 'job-listings.index') text-dark-blue @endif">
+                    <a href="{{ route('job-listings.index') }}">Jobs List</a>
+                </li>
+            </ul>
+
+            <a href="">
+                <x-button type="button" class="h-10 w-28 font-medium text-sm text-white">
+                    Login
+                </x-button>
+            </a>
+
+        </div>
+    </div>
+</div>
