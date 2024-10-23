d51c0a5ee6acc8b4d55d9d392ca2551927d11170
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-23 11:39:01 +0800

    apply filters to clear input in the login form

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,30 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-card class="p-4 pb-20 w-[500px] mx-auto my-[90px]">
+    <x-card class="pb-20 w-[500px] mx-auto my-[90px]">
         <h1 class="text-center text-2xl text-dark-blue font-semibold">
             Sign in to your account
         </h1>
 
-        <form action="{{ route('auth.store') }}" method="POST" class="mt-10">
+        <form x-data="" x-ref="login-filters" action="{{ route('auth.store') }}" method="POST" class="mt-10">
             @csrf
 
             <div class="flex flex-col">
-                <x-text-input icon="user" placeholder="Email" name="email" value="{{ old('email') }}" form-ref="filters"
+                <x-text-input icon="user" placeholder="Email" name="email" value="{{ old('email') }}"
+                    form-ref="login-filters"
                     class="h-12 mb-1 {{ $errors->has('email') ? 'border-red-600' : 'border-white-coffee' }}" />
                 @error('email')
                     <div class="text-red-600 text-xs">{{ $message }}</div>
                 @enderror
 
                 <x-text-input icon="key" placeholder="Password" type="password" name="password" value=""
-                    form-ref="filters"
+                    form-ref="login-filters"
                     class="h-12 mt-3 mb-1 {{ $errors->has('password') ? 'border-red-600' : 'border-white-coffee' }}" />
                 @error('password')
                     <div class="text-red-600 text-xs">{{ $message }}</div>
                 @enderror
             </div>
 
-            <div class="flex justify-between mt-3 mb-5">
+            <div class="flex justify-between mt-3 mb-10">
                 <div class="flex gap-2 items-center">
                     <input type="checkbox" name="remember" class="rounded-sm border border-dark-blue" />
                     <label for="remember" class="text-sm text-dark-blue">Remember me</label>
