5abc9e9d4154642affd72e72c8aabcd93f8bb746
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-24 22:38:12 +0800

    added another fillable in JobListingApplication model and refactor the JobListingApplicationController store logic

--- app/Http/Controllers/JobListingApplicationController.php
+++ app/Http/Controllers/JobListingApplicationController.php
@@ -22,11 +22,20 @@ class JobListingApplicationController extends Controller {
      * Store a newly created resource in storage.
      */
     public function store(JobListing $jobListing, Request $request) {
+        $this->authorize('apply', $jobListing);
+
+        $validatedData = $request->validate([
+            'expected_salary' => 'required|min:1|max:1000000',
+            'cv' => 'required|file|mimes:pdf|max:2048'
+        ]);
+
+        $file = $request->file('cv');
+        $path = $file->store('cvs', 'private');
+
         $jobListing->jobListingApplications()->create([
             'user_id' => $request->user()->id,
-            ...$request->validate([
-                'expected_salary' => 'required|min:1|max:1000000'
-            ])
+            'expected_salary' => $validatedData['expected_salary'],
+            'cv_path' => $path
         ]);
 
         return redirect()->route('job-listings.show', $jobListing)->with('success', 'Job Application Submitted.');
